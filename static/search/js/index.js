34aad1eaec7e49b606f0fb0e8d5a112955ca8618
shsdev <shsschlarb-dev@yahoo.de>
2015-04-13 14:10:48 +0200

    Loading

--- static/search/js/index.js
+++ static/search/js/index.js
@@ -260,4 +260,22 @@ $(document).on("click", "[id^=fileItem]", function() {
  });
  
  
- 
+function onReady(callback) {
+    var intervalID = window.setInterval(checkReady, 1000);
+
+    function checkReady() {
+        if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalID);
+            callback.call(this);
+        }
+    }
+}
+
+function show(id, value) {
+    document.getElementById(id).style.display = value ? 'block' : 'none';
+}
+
+onReady(function () {
+    show('page', true);
+    show('loading', false);
+});
