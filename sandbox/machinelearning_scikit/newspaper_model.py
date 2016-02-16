6bdd896714573cf44a6c195da19eddcffe8b3654
janrn <jan.roerden@ait.ac.at>
2016-02-16 13:06:11 +0100

    minor changes

--- sandbox/machinelearning_scikit/newspaper_model.py
+++ sandbox/machinelearning_scikit/newspaper_model.py
@@ -8,7 +8,8 @@ from sklearn.cross_validation import train_test_split
 from sklearn import metrics
 from sklearn.externals import joblib
 
-# The training data folder must be passed as first argument
+# The training data folder must be passed as first argument.
+# Subfolders are named according to the subcategories that are represented by the files found inside.
 languages_data_folder = '/home/janrn/Development/machinelearning/articles'
 dataset = load_files(languages_data_folder)
 
@@ -16,7 +17,7 @@ dataset = load_files(languages_data_folder)
 docs_train, docs_test, y_train, y_test = train_test_split(
     dataset.data, dataset.target, test_size=0.5)
 
-# TASK: Build a an vectorizer
+# Build a an vectorizer
 clf = Pipeline([('vect', TfidfVectorizer(max_df=0.9, min_df=2)),
                 ('clf', LinearSVC())])
 
@@ -24,21 +25,21 @@ clf = Pipeline([('vect', TfidfVectorizer(max_df=0.9, min_df=2)),
 clf.fit(docs_train, y_train)
 
 # fit pipeline on all the data (no test)
-#clf.fit(dataset.data, dataset.target)
+# clf.fit(dataset.data, dataset.target)
 
 # get category names
 # print dataset.target_names
 
 # save the model to disk
-#joblib.dump(clf, 'newspapers.pkl')
+# joblib.dump(clf, 'newspapers.pkl')
 
-# # predict outcome
+# predict outcome
 y_predicted = clf.predict(docs_test)
-#
+
 # # Print the classification report
 print(metrics.classification_report(y_test, y_predicted,
                                     target_names=dataset.target_names))
-#
-# # Plot the confusion matrix
+
+# Plot the confusion matrix
 cm = metrics.confusion_matrix(y_test, y_predicted)
 print(cm)
