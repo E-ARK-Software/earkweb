b6adfc6e0dd78ab484a2813ca29179c64bd1b8b2
shsdev <shsschlarb-dev@yahoo.de>
2016-05-11 17:56:35 +0200

    add docker solr

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,19 +1,33 @@
 #!/usr/bin/env bash
 
+MYSQL_DATA_DIRECTORY = "/tmp/earkweb-mysql-data"
+DB_PAUSE=15
+SERVICES_PAUSE=20
+
+echo "Building the images ..."
 docker-compose build
 
-mkdir /tmp/earkweb-mysql-data
-
+echo "Runing mysql database ..."
+mkdir $MYSQL_DATA_DIRECTORY
 docker run --name tmpdb -d -p 3306:3306 -v /tmp/earkweb-mysql-data:/var/lib/mysql earkdbimg &
 
-sleep 15
+echo "Pause $DB_PAUSE seconds ..."
+sleep $DB_PAUSE
 
+echo "Initialising database container ..."
 docker exec tmpdb /init.sh
 
+echo "Stopping database container ..."
 docker stop tmpdb
 
+echo "Starting services (docker-compose up) ..."
 docker-compose up &
 
-sleep 20
+echo "Pause $SERVICES_PAUSE seconds ..."
+sleep $SERVICES_PAUSE
 
+echo "Creating user ..."
 docker exec -it earkweb_1 python /earkweb/util/createuser.py eark user@email eark true
+
+echo "Creating solr core for storage area ..."
+docker exec -it --user=solr solr_1 bin/solr create_core -c earkstorage
