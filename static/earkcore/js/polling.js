7fe022604527d2ecc69317318c27a3a7b1745e41
shsdev <shsschlarb-dev@yahoo.de>
2016-05-17 14:14:51 +0200

    Batch processing interface

--- static/earkcore/js/polling.js
+++ static/earkcore/js/polling.js
@@ -1,31 +1,33 @@
 /**
  * polling.js
  *
- * Basic polling functions
+ * Basic functions for starting a task, polling the task status, and acting upon successful task execution.
  */
 
 
 /**
- * Function to get data requires "get_data_url" to be defined
+ * Function to get data requires "request_url" to be defined
  */
 var get_data_func = function() {
-   window.console.log("Get data request url: " + this.get_data_url);
+   window.console.log("Get data request url: " + this.request_url);
 
-
-   $.ajax({
-    url: this.get_data_url,
-    method: "POST",
-    async: true,
-    data: get_data_params,
-    success: function(resp_data) {
+   var success_func = function(resp_data) {
      if(resp_data.success) {
          window.console.log("Task accepted, task id: " + resp_data.id);
-         pollstate(resp_data.id);
+
+         pollstate(resp_data.id, this.success_func, this.poll_request_url);
      } else {
         window.console.log(resp_data.errmsg);
         window.console.log(resp_data.errdetail);
      }
-    }
+    }.bind(this);
+
+   $.ajax({
+    url: this.request_url,
+    method: "POST",
+    async: true,
+    data: this.request_params,
+    success: success_func,
    });
    // only execute ajax request, do not submit form
    return false;
@@ -37,7 +39,7 @@ var get_data_func = function() {
  * Requires "poll_request_url" to be defined and a success function poll_success_func(resp_data.result) to process the
  * result in case of success.
  */
-function pollstate(in_task_id) {
+function pollstate(in_task_id, success_func, poll_request_url) {
     var ready = false;
     $(document).ready(function() {
           var PollState = function(task_id) {
@@ -55,11 +57,11 @@ function pollstate(in_task_id) {
                               window.console.log("Task status: success");
                               window.console.log("Result: " + resp_data.result);
                               var json_result = JSON.parse(resp_data.result);
-                              poll_success_func(json_result);
+                              success_func(json_result);
                               ready = true;
                           } else if(resp_data.state == 'PENDING') {
                                 // check again if task still pending
-                                setTimeout(function(){ pollstate(task_id) }, 3000);
+                                setTimeout(function(){ pollstate(task_id, success_func, poll_request_url) }, 3000);
                           } else {
                             window.console.log("In progress ...");
                           }
