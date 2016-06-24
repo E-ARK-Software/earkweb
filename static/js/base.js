0ff915f6e0faa49dc30ce36a736f87254796a478
shsdev <shsschlarb-dev@yahoo.de>
2016-06-24 14:20:34 +0200

    Batch ingest: define SIP package name to AIP identifier mapping

--- static/js/base.js
+++ static/js/base.js
@@ -63,3 +63,12 @@ function show(id, value) {
  */
 function name_to_id(str) { return  str.replace(/[|&;$%@"<>()+,./\- ]/g, "").toLowerCase(); }
 
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
