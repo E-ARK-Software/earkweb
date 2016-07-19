ab01eb7d36881064de790a0dfc11630d0e95f5ec
shsdev <shsschlarb-shsdev@yahoo.de>
2016-07-19 09:26:14 +0200

    reset repository script

--- util/cleanup.sh
+++ util/cleanup.sh
@@ -3,15 +3,34 @@
 MYSQL_USERNAME=arkiv
 MYSQL_PASSWORD=arkiv
 
-# Cleanup woking dir
-rm -rf /var/data/earkweb/work/*
-rm -rf /var/data/earkweb/storage/pairtree_root/*
-rm -rf /var/data/earkweb/reception/*
-# Update status in DB
-echo "delete from earkcore_informationpackage;" | mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD eark
-echo "delete from search_aip;" | mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD eark
-echo "delete from search_dip;" | mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD eark
-echo "delete from search_inclusion;" | mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD eark
+repo_data_directory="/var/data/earkweb"
 
-# delete index
-curl -X GET "http://localhost:8983/solr/earkstorage/update?stream.body=%3Cdelete%3E%3Cquery%3E*%3C/query%3E%3C/delete%3E&commit=true"
+echo "Do you want to delete repository data? (y/n, default: y)"
+read actiondata
+actiondata_val=${actiondata:-y}
+if [[ "$actiondata_val" = "y" ]]; then
+    if [ ! -z "$repo_data_directory" -a "$repo_data_directory" != " " ]; then
+        rm -rf $repo_data_directory
+	rm -rf $repo_data_directory/work/*
+	rm -rf $repo_data_directory/storage/pairtree_root/*
+	rm -rf $repo_data_directory/reception/*
+    fi
+fi
+
+echo "Do you want to delete database data? (y/n, default: y)"
+read actiondata
+actiondata_val=${actiondata:-y}
+if [[ "$actiondata_val" = "y" ]]; then
+	echo "delete from earkcore_informationpackage;" | mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD eark
+	echo "delete from search_aip;" | mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD eark
+	echo "delete from search_dip;" | mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD eark
+	echo "delete from search_inclusion;" | mysql -u $MYSQL_USERNAME -p$MYSQL_PASSWORD eark
+fi
+
+echo "Do you want to reset the index? (y/n, default: y)"
+read actiondata
+actiondata_val=${actiondata:-y}
+if [[ "$actiondata_val" = "y" ]]; then
+	curl -X GET "http://localhost:8983/solr/earkstorage/update?stream.body=%3Cdelete%3E%3Cquery%3E*%3C/query%3E%3C/delete%3E&commit=true"
+fi
+
