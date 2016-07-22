9c7cd152f5b6bc44df77174667445752f88a6cf0
shsdev <shsschlarb-dev@yahoo.de>
2016-07-22 09:19:00 +0200

    REST client for posting a file improved

--- earkcore/rest/restclient.py
+++ earkcore/rest/restclient.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 # coding=UTF-8
+import logging
+import os
 from earkcore.rest.restendpoint import RestEndpoint
-# test_client.py
 from poster.encode import multipart_encode
 from poster.streaminghttp import register_openers
 import urllib2
+logger = logging.getLogger(__name__)
+from config.configuration import root_dir
 
 __author__ = "Sven Schlarb"
-__copyright__ = "Copyright 2015, The E-ARK Project"
+__copyright__ = "Copyright 2016, The E-ARK Project"
 __license__ = "GPL"
 __version__ = "0.0.1"
 
@@ -32,37 +35,48 @@ class RestClient(object):
     def post_file(self, resource, field_name, file_path):
         """
         Post file
-        @type       str: string
-        @param      str: Some string
+        @type       resource: string
+        @param      resource: Resource path of the REST request
+        @type       field_name: string
+        @param      field_name: Field name of the file to be posted
+        @type       file_path: string
+        @param      file_path: Path to the file to be posted
         @rtype:     bool
-        @return:    Validity
+        @return:    Indicates if the post request was successful
         """
         uri = self.rest_endpoint.get_resource_uri(resource)
-        #field:rdf	abc.ttl --> abc "name"
-
-        register_openers()
-
-        datagen, headers = multipart_encode({field_name: open(file_path, "r")})
-
-        request = urllib2.Request(uri, datagen, headers)
-
-        response = urllib2.urlopen(request)
-        print response
-
-        return True
+        try:
+            register_openers()
+            datagen, headers = multipart_encode({field_name: open(file_path, "r")})
+            request = urllib2.Request(uri, datagen, headers)
+            urllib2.urlopen(request)
+            logger.info("Successfully posted file: %s to %s" % (file_path, uri))
+            return True
+        except IOError, e:
+            logger.error("An error occurred: %s " % e.message)
+            return False
+        except urllib2.HTTPError, e:
+            logger.error("An error occurred: %s (%d)" % (e.reason, e.code))
+            return False
+        except urllib2.URLError, e:
+            logger.error("An error occurred: %s" % e.reason)
+            return False
 
 
 class RestClientTest(unittest.TestCase):
+
     from config.configuration import peripleo_server_ip, peripleo_port, peripleo_path
     rest_endpoint = RestEndpoint("http://%s:%s" % (peripleo_server_ip, peripleo_port), peripleo_path)
     my_instance = RestClient(rest_endpoint)
 
     def test_do_post(self):
         """
-        Must return true
+        Test post request
         """
-        test_file = "/var/data/earkweb/work/1f1576d2-fcea-43be-a0c9-7051f7116ae6/representations/peripleottl/data/ob_1994.ttl"
-        print RestClientTest.my_instance.post_file("admin/gazetteers", "rdf", test_file)
+        # Test deactivated because it requires a running Peripleo instance
+        pass
+        #test_file = os.path.join(root_dir, "earkresources/geodata/ob_2015.ttl")
+        #print RestClientTest.my_instance.post_file("admin/gazetteers", "rdf", test_file)
 
 
 if __name__ == '__main__':
