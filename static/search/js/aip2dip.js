803249413a6a0c172406cbbdd48a87a7474164b9
shsdev <shsschlarb-dev@yahoo.de>
2016-08-09 17:05:39 +0200

    Enable filtering in DIP overview

--- static/search/js/aip2dip.js
+++ static/search/js/aip2dip.js
@@ -1,3 +1,22 @@
+/**
+ * Update data table
+ * current_ip variable must be provided (global scope)
+ */
+function updateTable(filterword) {
+    $.ajax({
+        url: "/earkweb/search/ips_table",
+        type: "POST",
+        data: "filterword=" + filterword,
+    }).success(function(table_html){
+        $('#ips-table').html(table_html);
+    });
+}
+$( document ).ready(function() {
+    $( "#filterchars" ).keyup(function() {
+       updateTable($('#filterchars').val());
+       window.console.log($('#filterchars').val());
+    });
+});
 $(document).ready(function () {
         $(".nav li").removeClass("active");
         $('#aip2dip').addClass('active');
