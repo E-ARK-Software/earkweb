3b5f32754d8dcd922eac13460ec5d10f93632b41
shsdev <sschlarb@gmail.com>
2016-11-08 16:45:48 +0100

    DIP creation API: test script modified

--- util/testdipapi.sh
+++ util/testdipapi.sh
@@ -1,25 +1,17 @@
 #!/usr/bin/env bash
+
 echo "DIP PREPARE"
 RESP=`curl -s -X POST -d '{"process_id": "c1b1c16e-2c00-474f-b99b-42019b3eaeed"}' http://localhost:8000/earkweb/search/prepareDIPWorkingArea`
 echo $RESP
-
+# status
+sleep 3
 JOBID=`echo $RESP | awk -F"," '{print $4}' | awk -F":" '{print $2}' | sed 's/ //g;s/\}//g;s/\"//g'`
-for i in {1..9}; do
-    echo `curl -s http://localhost:8000/earkweb/search/jobstatus/$JOBID`
-    sleep 2
-done
+echo `curl -s http://localhost:8000/earkweb/search/jobstatus/$JOBID`
 
-#sleep 1
-#echo `curl -s http://localhost:8000/earkweb/search/jobstatus/$JOBID`
-#
-#sleep 5
-#
-#echo "DIP CREATE"
-#RESP2=`curl -s -X POST -d '{"process_id": "c1b1c16e-2c00-474f-b99b-42019b3eaeed"}' http://localhost:8000/earkweb/search/createDIP`
-#echo $RESP2
-#
-#JOBID2=`echo $RESP2 | awk -F"," '{print $4}' | awk -F":" '{print $2}' | sed 's/ //g;s/\}//g;s/\"//g'`
-#echo $JOBID2
-#echo `curl -s http://localhost:8000/earkweb/search/jobstatus/$JOBID2`
-#sleep 1
-#echo `curl -s http://localhost:8000/earkweb/search/jobstatus/$JOBID2`
+echo "DIP CREATE"
+RESP2=`curl -s -X POST -d '{"process_id": "c1b1c16e-2c00-474f-b99b-42019b3eaeed"}' http://localhost:8000/earkweb/search/createDIP`
+echo $RESP2
+# status
+sleep 3
+JOBID2=`echo $RESP2 | awk -F"," '{print $4}' | awk -F":" '{print $2}' | sed 's/ //g;s/\}//g;s/\"//g'`
+echo `curl -s http://localhost:8000/earkweb/search/jobstatus/$JOBID2`
