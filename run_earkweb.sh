4940ea8600fba785f7b472bedac42c658c676195
Sven Schlarb <sven.schlarb@ait.ac.at>
2021-12-14 16:16:39 +0100

    PROHUB deployment adaptions

--- run_earkweb.sh
+++ run_earkweb.sh
@@ -2,4 +2,4 @@
 echo "Starting earkweb ..."
 #cd /earkweb && python3 manage.py runserver 0.0.0.0:8000 
 #cd /earkweb && uwsgi --http :8000 --wsgi-file earkweb.py
-cd /earkweb && uwsgi --ini docker/uwsgi/earkweb.ini
+cd /home/shs/earkweb && uwsgi --ini /home/shs/earkweb/earkweb.ini
