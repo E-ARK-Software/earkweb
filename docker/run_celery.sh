5f3f7dc433e611a0b1ceedd993e519513ec394f6
Sven Schlarb <sven.schlarb@ait.ac.at>
2021-12-15 20:06:51 +0100

    Move docker related run scripts

--- docker/run_celery.sh
+++ docker/run_celery.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sleep 20
 echo "Starting celery ..."
 # cd /earkweb && celery multi start ingestqueue -A earkweb.celery --concurrency=4 -Ofair --pidfile=/data/celery_worker.pid  --logfile=/data/celery_default_queue.log
 # cd /earkweb && celery -A earkweb.celery worker --pool threads -Ofair --pidfile=/data/celery_worker.pid  --logfile=/data/celery_default_queue.log &
