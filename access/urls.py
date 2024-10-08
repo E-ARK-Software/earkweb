2129aebfb68284f685b589ba1d7e84f1f00f6335
Sven Schlarb <shsdev@posteo.net>
2024-10-08 11:08:15 +0200

    correct access url pattern

--- access/urls.py
+++ access/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     re_path(r'^search$', views.search, name='search'),
     re_path(r'^get-item/(?P<identifier>[a-zA-Z0-9\_\-\:\.,=+]{20,200})/(?P<entry>[0-9a-zA-Z_\-/\. \:]{3,500})/$',
         views.get_information_package_item, name='access_aip_item'),
-    re_path(r'^(?P<identifier>[a-zA-Z0-9\_\-\:\.,=+]{20,200})/$', views.InformationPackageDetail.as_view(), name='asset'),
+    re_path(r'^(?P<identifier>[a-zA-Z0-9\_\-\:\.,/=+]{10,500})/$', views.InformationPackageDetail.as_view(), name='asset'),
     re_path(r'^indexing-status$', views.indexingstatus, name='indexing-status'),
     re_path(r'^reindex-storage/$', views.reindex_storage, name='reindex-storage'),
 ]
