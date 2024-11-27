97f681303f6c5ed1078bc79bd7fbce878e575cbe
Sven Schlarb <shsdev@posteo.net>
2024-11-27 12:45:59 +0100

    Adapt virtual environment path

--- start_flower.sh
+++ start_flower.sh
@@ -6,5 +6,5 @@ flower_user=$(get_config_value "$config_file" "server" "flower_user")
 flower_password=$(get_config_value "$config_file" "server" "flower_password")
 
 echo "Starting flower ..."
-source ./.venv/bin/activate
+source ./venv/bin/activate
 celery -A earkweb.celery flower --basic_auth=${flower_user}:${flower_password} --url_prefix=flower --port=5555
